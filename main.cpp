05964f9ed817c0b4044598e5086939d69919f6b4
carsoncrum <carsoncrum07@gmail.com>
2024-09-12 17:29:51 +0000

    Initial Version of 2.14 program.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+// Display my initials
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main()
+{
+  char first, middle, last;
+  first = 'C';
+  middle = 'W';
+  last = 'C';
+  cout << first << middle << last << endl;
+  return 0;
+}
