5aada4fd5b8b62c3e18e21885bc6eb9e4618903f
carsoncrum <carsoncrum07@gmail.com>
2024-09-12 17:34:57 +0000

    Cleared out code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-// This is the main branch for three separate programs on their own branches
+
 #include <iostream>
 using namespace std;
 
-int main() { cout << "Chapter 2 Checkpoint Programs\n"; }
+int main()
+{
+
+}
