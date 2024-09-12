596184821b7ab4d78ea87d41b64e3a8c02c905a7
carsoncrum <carsoncrum07@gmail.com>
2024-09-12 17:06:30 +0000

    Added comments and modified cout statement

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// This is the main branch for three separate programs on their own branches
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main() { cout << "Chapter 2 Checkpoint Programs\n"; }
