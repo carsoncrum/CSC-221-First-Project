dfa2fda5a150522c38cbb68452fdf0f82fda3e78
carsoncrum <carsoncrum07@gmail.com>
2024-09-12 17:52:17 +0000

    Added code for Chapter 2.17

--- .ccls-cache/@home@runner@CSC-221-First-Project/main.cpp
+++ .ccls-cache/@home@runner@CSC-221-First-Project/main.cpp
@@ -1,8 +1,14 @@
-
+//display name, address, and phone number on seperate strings
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
- 
+  
+ string name = "Carson Crum";
+ string address = "28 Paddock Lane";
+ string phone = "757-968-9978";
+  cout << name << address
+  return 0;
 }
