43be1b8900c99e35e100071b482ec177fe3cd20f
carsoncrum <carsoncrum07@gmail.com>
2024-09-12 17:33:44 +0000

    Cleared out code

--- .ccls-cache/@home@runner@CSC-221-First-Project/main.cpp
+++ .ccls-cache/@home@runner@CSC-221-First-Project/main.cpp
@@ -1,13 +1,8 @@
-// Display my initials
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  char first, middle, last;
-  first = 'C';
-  middle = 'W';
-  last = 'C';
-  cout << first << middle << last << endl;
-  return 0;
+ 
 }
