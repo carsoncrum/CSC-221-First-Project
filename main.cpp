e7ba72f5a18880358f6973631e4a5b56ca7ca904
carsoncrum <carsoncrum07@gmail.com>
2024-09-12 17:58:34 +0000

    Added code for Chapter 2.20

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-
+//display age and weight
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
+  int age = 17;
+  float weight = 200;
+ cout << "My age is " << age << " and my weight is " << weight << " pounds." << endl;
+  return 0;
 }
